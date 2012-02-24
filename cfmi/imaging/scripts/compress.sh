3765ab7443499789bd065639564547a771b6b22e
Shawn Nock <nock@nocko.org>
2012-02-24 11:43:04 -0500

    Updates series naming routine. Contributed by Paul

--- cfmi/imaging/scripts/compress.sh
+++ cfmi/imaging/scripts/compress.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ #!/bin/bash
 
 args=("$@")
 SUBJECT=$1
@@ -6,7 +6,7 @@ FILENAME=$3/$1.$2
 EXTEN=$2
 EMAIL=$4
 TMPDIR=`mktemp -d`
-PATHS=`curl -f -k https://imaging.cfmi.georgetown.edu/api/path/${SUBJECT}`
+PATHS=`curl -f -k https://imaging.cfmi.georgetown.edu/api/path/${SUBJECT} | sed 's_exports_glusterfs/mirror/cfmi_g'`
 TEMPLATE=$HOME/cfmi/cfmi/imaging/templates/email.tpl
 
 cd $TMPDIR;
@@ -19,28 +19,51 @@ if [ ${EXTEN::3} == "nii" ]; then
 else
     index=1
     dscache=""
-    for path in $PATHS; do
-	if [ -d ${path}/1.ACQ/ ]; then
-	    firstfile=`ls ${path}/1.ACQ/ | head -1`
-	    rootname=`strings ${path}/1.ACQ/${firstfile} | grep tProtocolName | awk -F'=' '{print $2}' | sed 's/"//g' | sed 's/ //g' | sed 's/+/_/g' | sed 's/\///g'`
-	else
-	    firstfile=`ls ${path}/5.ACQ/ | head -1`
-	    rootname=`strings ${path}/5.ACQ/${firstfile} | grep tProtocolName |  awk -F'=' '{print $2}' | sed 's/"//g' | sed 's/ //g' | sed 's/+/_/g' | sed 's/\///g'`
-	fi
-	if [ -z "$rootname" ]; then
-	    continue
-	fi
-	datestring=`echo ${path} | awk -F"/" '{print $8 $9 $10}'`
-	if [ "$datestring" != "$dscache" ]; then
+    
+    for imgpath in ${PATHS[*]}; do
+        firstrawdir=`ls ${imgpath} | head -1`
+        firstrawfile=`ls ${imgpath}/${firstrawdir}/ | head -1`
+        names=(`strings ${imgpath}/${firstrawdir}/${firstrawfile} | grep tProtocolName | awk -F'=' '{print $2}' | sed 's/"//g' | sed 's/ //g' | sed 's/+/_/g' | sed 's/\///g' | sed 's/,///g'`)
+        # this returns the penultimate element in ${names[*]}; why did I want to do this?? This is a tricky thing to do, so I can only imagine I had a good reason for it :/
+        rootname=${names[${#names[*]}-1]}
+        
+        datestring=`echo ${imgpath} | awk -F"/" '{print $10 $11 $12}'`
+        if [ "$datestring" != "$dscache" ]; then
 	    index=1
 	    dscache=$datestring
 	fi
-	nameNdate="${rootname}_${datestring}"
-	if [ -n "$rootname" ]; then
-	    ln -s ${path} "${nameNdate}_${index}"
+        nameNdate="${rootname}_${datestring}"
+        if [ -n "$rootname" ]; then
+	    ln -s ${imgpath} "${nameNdate}_${index}"
 	    index=$(($index+1))
 	fi
     done
+
+    ##################################   old code  #############
+    #for path in $PATHS; do
+	#if [ -d ${path}/1.ACQ/ ]; then
+	#    firstfile=`ls ${path}/1.ACQ/ | head -1`
+	#    rootname=`strings ${path}/1.ACQ/${firstfile} | grep tProtocolName | awk -F'=' '{print $2}' | sed 's/"//g' | sed 's/ //g' | sed 's/+/_/g' | sed 's/\///g'`
+	#else
+	#    firstfile=`ls ${path}/5.ACQ/ | head -1`
+	#    rootname=`strings ${path}/5.ACQ/${firstfile} | grep tProtocolName | awk -F'=' '{print $2}' | sed 's/"//g' | sed 's/ //g' | sed 's/+/_/g' | sed 's/\///g'`
+	#fi
+	#if [ -z "$rootname" ]; then
+	#    continue
+	#fi
+	#datestring=`echo ${path} | awk -F"/" '{print $8 $9 $10}'`
+	#if [ "$datestring" != "$dscache" ]; then
+	#    index=1
+	#    dscache=$datestring
+	#fi
+	#nameNdate="${rootname}_${datestring}"
+	#if [ -n "$rootname" ]; then
+	#    ln -s ${path} "${nameNdate}_${index}"
+	#    index=$(($index+1))
+	#fi
+    #done
+    #####################################################
+
     cd $TMPDIR
 fi
 
